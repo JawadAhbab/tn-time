fc5839ee9e8190934ba6a623df9b72cc6393bd88
JawadAhbab <jawad.ahbab.turna@gmail.com>
2025-04-25 14:48:34 +0600

    🔥 Time Gap

--- examples/example.ts
+++ examples/example.ts
@@ -1,4 +1,10 @@
 import { time } from '../src/index'
 console.clear()
 
-console.log(time(time().shift(0.99999, 'yr')).gap({ maxClause: 2 }))
+console.log(
+  time(time().shift(1.002, 'day')).gap({
+    maxClause: 3,
+    clauses: ['day', 'hr', 'min'],
+    // trimBlankClause: true,
+  })
+)
--- src/core/timeGap/accessories/timeGapLinter.ts
+++ src/core/timeGap/accessories/timeGapLinter.ts
@@ -1,9 +1,9 @@
+import { clauseValue } from '../../../accessories/clauseValue'
 import { TimegapReadyOpts } from './TimeGap'
 import { TimeGapAmount } from './timeGapAmounts'
 
 export const timeGapLinter = (amounts: TimeGapAmount[], opts: TimegapReadyOpts) => {
-  const { decimal } = opts
-  console.log(amounts)
+  const { decimal, lastBlankClause, trimBlankClause } = opts
   let linted = amounts
 
   const last = linted[linted.length - 1]
@@ -13,15 +13,22 @@ export const timeGapLinter = (amounts: TimeGapAmount[], opts: TimegapReadyOpts) 
   function stackup() {
     if (linted.length <= 1) return
     const last = linted[linted.length - 1]
-    console.log(last)
-
-    function stack() {
-      linted[linted.length - 2].number++
-      linted.pop()
-      stackup()
-    }
+    const last2nd = linted[linted.length - 2]
+    const maxnum = clauseValue[last2nd.clause] / clauseValue[last.clause]
+    if (last.number < maxnum) return
+    last2nd.number++
+    linted.pop()
+    stackup()
   }
 
-  console.log(linted)
+  if (!lastBlankClause) removeLastBlanks()
+  function removeLastBlanks() {
+    const last = linted[linted.length - 1]
+    if (last.number) return
+    linted.pop()
+    removeLastBlanks()
+  }
+
+  if (trimBlankClause) linted = linted.filter(i => i.number)
   return linted
 }
