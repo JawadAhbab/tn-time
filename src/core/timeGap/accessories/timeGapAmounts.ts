eefdfa386b90b7694f1f89c9c76075e640c63151
JawadAhbab <jawad.ahbab.turna@gmail.com>
2025-04-25 10:28:49 +0600

    🔥 Time Gap

--- src/core/timeGap/accessories/timeGapAmounts.ts
+++ src/core/timeGap/accessories/timeGapAmounts.ts
@@ -3,27 +3,31 @@ import { TimeClause } from '../../../accessories/TimeClause'
 import { TimegapFormats } from './TimeGap'
 export type TimeGapAmount = { number: number; clause: keyof TimegapFormats }
 
-export const timeGapAmounts = (gapms: number, clauses: TimeClause[], amounts: TimeGapAmount[] = [], currClauseCount = 0): TimeGapAmount[] => {
+export const timeGapAmounts = (ms: number, clauses: TimeClause[], amounts: TimeGapAmount[] = [], currClauseCount = 0): TimeGapAmount[] => {
   ++currClauseCount
   const lastkey = clauses[clauses.length - 1] ?? 'yr'
 
   let amount: TimeGapAmount
-  if ((gapms >= conv.yr && clauses.includes('yr')) || lastkey === 'yr') {
-    amount = { number: gapms / conv.yr, clause: 'yr' }
-  } else if ((gapms >= conv.mo && clauses.includes('mo')) || lastkey === 'mo') {
-    amount = { number: gapms / conv.mo, clause: 'mo' }
-  } else if ((gapms >= conv.day && clauses.includes('day')) || lastkey === 'day') {
-    amount = { number: gapms / conv.day, clause: 'day' }
-  } else if ((gapms >= conv.hr && clauses.includes('hr')) || lastkey === 'hr') {
-    amount = { number: gapms / conv.hr, clause: 'hr' }
-  } else if ((gapms >= conv.min && clauses.includes('min')) || lastkey === 'min') {
-    amount = { number: gapms / conv.min, clause: 'min' }
-  } else if ((gapms >= conv.sec && clauses.includes('sec')) || lastkey === 'sec') {
-    amount = { number: gapms / conv.sec, clause: 'sec' }
+  if ((ms >= conv.yr && clauses.includes('yr')) || lastkey === 'yr') {
+    amount = { number: ms / conv.yr, clause: 'yr' }
+  } else if ((ms >= conv.mo && clauses.includes('mo')) || lastkey === 'mo') {
+    amount = { number: ms / conv.mo, clause: 'mo' }
+  } else if ((ms >= conv.day && clauses.includes('day')) || lastkey === 'day') {
+    amount = { number: ms / conv.day, clause: 'day' }
+  } else if ((ms >= conv.hr && clauses.includes('hr')) || lastkey === 'hr') {
+    amount = { number: ms / conv.hr, clause: 'hr' }
+  } else if ((ms >= conv.min && clauses.includes('min')) || lastkey === 'min') {
+    amount = { number: ms / conv.min, clause: 'min' }
+  } else if ((ms >= conv.sec && clauses.includes('sec')) || lastkey === 'sec') {
+    amount = { number: ms / conv.sec, clause: 'sec' }
   } else {
-    amount = { number: gapms, clause: 'msec' }
+    amount = { number: ms, clause: 'msec' }
   }
 
   amounts.push(amount)
   return amounts
+}
+
+const getCaluseAmount = (ms: number, clause: TimeClause): TimeGapAmount => {
+  return { number: ms / conv[clause], clause }
 }
