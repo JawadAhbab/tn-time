2db02f92e035d030b1b0021ba26f1cb3e7135aab
JawadAhbab <jawad.ahbab.turna@gmail.com>
2025-04-25 09:37:14 +0600

    🔥 Time Gap

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -178,8 +178,7 @@ const timeFormat = function (date, format) {
       day: '2-digit',
       hour: '2-digit',
       minute: '2-digit',
-      second: '2-digit',
-      fractionalSecondDigits: 3
+      second: '2-digit'
     }));
   }
   const formatarr = timeFormatArr(format ?? 'dd-mm-Y');
--- src/core/timeFormat/timeFormat.ts
+++ src/core/timeFormat/timeFormat.ts
@@ -15,7 +15,6 @@ export const timeFormat: TimeFormat = (date, format, opts = {}) => {
         hour: '2-digit',
         minute: '2-digit',
         second: '2-digit',
-        fractionalSecondDigits: 3,
       })
     )
   }
